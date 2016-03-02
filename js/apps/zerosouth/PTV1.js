318e04aa58efac4303acbb8dd7c0202fbe5b9a54
Kai Salmen <salmen@t-online.de>
2016-03-02 22:17:56 +0100

    Updated browser compatibility warnings and alerts

--- js/apps/zerosouth/PTV1.js
+++ js/apps/zerosouth/PTV1.js
@@ -6,10 +6,13 @@
 
 KSX.apps.zerosouth.PTV1 = {
     glob : {
+        go : false,
         appLifecycle : new KSX.apps.core.AppLifecycle("App Lifecycle for PTV 1 loader")
     },
     func : {
         init : function () {
+            window.addEventListener( 'resize', KSX.apps.zerosouth.PTV1.func.onWindowResize, false );
+
             var impl = new KSX.apps.zerosouth.impl.PTV1Loader(document.getElementById("DivGLFullCanvas"));
             KSX.apps.zerosouth.PTV1.glob.appLifecycle.addSceneApp(impl.sceneApp);
 
@@ -32,18 +35,35 @@ if (bowser.msie) {
 else if (bowser.msedge && bowser.version < 13.10586) {
     alert('Edge ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Edge 13+.');
 }
-else if (bowser.safari && bowser.version < 9.0) {
-    alert('Safari ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Safari 9+.');
+else if (bowser.safari) {
+    if (bowser.version < 9.0) {
+        alert('Safari ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Safari 9+.');
+    }
+    else {
+        alert('Safari is considered unstable. It may or may not work (WIP)...');
+        KSX.apps.zerosouth.PTV1.glob.go = true;
+    }
 }
 else if (bowser.chrome && bowser.version < 46.0) {
     alert('Safari ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Safari 9+.');
 }
-else if (bowser.firefoc && bowser.version < 42.0) {
-    alert('Safari ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Safari 9+.');
+else if (bowser.firefox) {
+    if (bowser.version < 42.0) {
+        alert('Safari ' + bowser.version + ' is not supported!\nPlease use Chrome 46+, Firefox 42+ or Safari 9+.');
+    }
+    else if (bowser.mobile) {
+        alert('Mobile Firefox is considered unstable, but it may work');
+        KSX.apps.zerosouth.PTV1.glob.go = true;
+    }
+    else {
+        KSX.apps.zerosouth.PTV1.glob.go = true;
+    }
 }
 else {
-    window.addEventListener( 'resize', KSX.apps.zerosouth.PTV1.func.onWindowResize, false );
+    KSX.apps.zerosouth.PTV1.glob.go = true;
+}
 
+if (KSX.apps.zerosouth.PTV1.glob.go) {
     KSX.apps.zerosouth.PTV1.func.init();
     KSX.apps.zerosouth.PTV1.func.render();
 }
